ff1f32676dee3d23d5722ff35f6dd2ddfbec665c
Denny Kurniawan <dennykurniawanjr@gmail.com>
2021-05-03 23:34:28 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -6,21 +6,16 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserHistory } from 'history'
 
 localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMywiZXhwIjoxNjIwMzgwNTA1fQ.tZpUAezlArp7z6uHFfwDmae9xI8IxY4oCKSIyNuaeas')
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-export const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL
-})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
