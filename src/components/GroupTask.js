d2fa498903500f7b291fa3c662077a47230d25c4
Denny Kurniawan <dennykurniawanjr@gmail.com>
2021-05-03 16:02:34 +0700

    group task sizing

--- src/components/GroupTask.js
+++ src/components/GroupTask.js
@@ -1,19 +1,37 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import Container from 'react-bootstrap/Container'
 
 const GroupTask = () => {
-    const [group, setGroup] = useState([])
+    const colors = [
+        { 'color1': '#EB2F96', 'color2': '#FFF9FB'},
+        { 'color1': '#7B61FF', 'color2': '#FCFAFD'},
+        { 'color1': '#2F54EB', 'color2': '#F7FAFF'},
+        { 'color1': '#52C41A', 'color2': '#F8FEF1'}
+    ]
     
-    useEffect(() => {
-        axios.get('')
+    // useEffect(() => {
+    //     axios.get('')
         
         
-    })
+    // })
 
     return (
-        <div>
-            
+        <div className="content">
+            <h5>Product Roadmap</h5>
+            <div className="d-flex flex-wrap justify-content-between">
+            {
+                colors.map(color => {
+                    const { color1, color2 } = color
+                    return (
+                        <div className="group" style={{backgroundColor: color2, border: `1px solid ${color1}`}}>
+                            ABC
+                        </div>
+                    )
+                })
+            }
+            </div>
         </div>
     )
 }
--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import GroupTask from '../components/GroupTask'
+import Col from 'react-bootstrap/Col'
 
 const ToDoPage = () => {
     return (
-        <div>
-            <Sidebar />
+        <div className="bg">
+            <Col md="1" className="logo">
+                <Sidebar />
+            </Col>
+            <div className="min-vh-100">
+                <GroupTask />
+            </div>
         </div>
     )
 }
